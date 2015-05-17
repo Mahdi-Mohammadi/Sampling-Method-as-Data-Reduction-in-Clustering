6acabf15df62a909a4a00589314b94cd60e549c4
Sir-M7 <sir.m77@gmail.com>
2015-05-17 16:12:47 +0430

    add type of selection

--- StratifiedRankedSetSampling.m
+++ StratifiedRankedSetSampling.m
@@ -1,4 +1,10 @@
-function [ data,varAvg,stdAvg,meanAvg ] = StratifiedRankedSetSampling(dataWithClassName,samplingLoop,stratifyNumber,sampleSize )
+function [ data,varAvg,stdAvg,meanAvg ] = StratifiedRankedSetSampling(dataWithClassName,samplingLoop,stratifyNumber,sampleSize,type )
+% type of sampling : 
+%                   - 1 = Diagonal
+%                   - 2 = Middle
+%                   - 3 = First point
+%                   - 4 = Last point
+    
 dataWithClassName = sort(dataWithClassName);
 stratifiedIndex = quantile(dataWithClassName,(1:stratifyNumber-1)/stratifyNumber);
 dataMean = zeros(samplingLoop,1);
@@ -19,8 +25,20 @@ for i = 1:samplingLoop
     tempDataStratified = dataWithClassName(tempIndex,:);
     for j = 1:sampleSize
         tempDataRanked = sort(tempDataStratified(randsample(1:length(tempDataStratified), sampleSize, true),:));
-        selectedData(j,1) = tempDataRanked(j,1);
-        selectedData(j,2) = tempDataRanked(j,2);
+        if type == 1 % diagonal
+            selectedData(j,1) = tempDataRanked(j,1);
+            selectedData(j,2) = tempDataRanked(j,2);
+        elseif type == 2 % middle
+            middle = round(length(tempDataRanked(:,1))/2);
+            selectedData(j,1) = tempDataRanked(middle,1);
+            selectedData(j,2) = tempDataRanked(middle,2);
+        elseif type == 3 % first point
+            selectedData(j,1) = tempDataRanked(1,1);
+            selectedData(j,2) = tempDataRanked(1,2);
+        elseif type == 4 % last point
+            selectedData(j,1) = tempDataRanked(length(tempDataRanked(:,1)),1);
+            selectedData(j,2) = tempDataRanked(length(tempDataRanked(:,1)),2);
+        end
     end
     dataMean(i) = mean(selectedData(:,1));
     dataAll(1:sampleSize,i) = selectedData(:,1);
